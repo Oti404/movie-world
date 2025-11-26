b9c3cc9737d1b223a1d731b6ef12caa2ec45f842
priscu vlad <vlad.priscu@globant.com>
2025-11-26 12:31:12 +0200

    project setup

---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+watch(
+  () => route.params.id,
+  (newId, oldId) => {
+    console.log(newId, oldId)
+  }
+)
+console.log(route.params)
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <div class="flex">
+    <div>asdasd</div>
+    <div>asdasd2</div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+function f(val:string) {
+
+}
+f(1)
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
