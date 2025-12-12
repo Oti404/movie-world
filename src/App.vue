c9093abf3a04d4fadea00bdededbb7b9e3f2846c
priscu vlad <vlad.priscu@globant.com>
2025-12-12 14:47:56 +0200

    project base route and movie detail + store

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,27 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import Search from "./components/Search.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+  <div class="min-h-screen bg-black text-white font-sans">
+    <header class="flex justify-between items-center p-4 bg-gradient-to-b from-black via-gray-900 to-transparent">
+      <RouterLink to="/"><h1 class="text-3xl font-bold text-red-600">MovieApp</h1></RouterLink>
+      <nav class="flex gap-6">
+        <Search></Search>
+        <RouterLink class="hover:underline" to="/upcoming" v-if="false">Upcoming</RouterLink>
+        <button class="text-sm bg-red-600 px-4 py-1 rounded hover:bg-red-700">Login</button>
       </nav>
-    </div>
-  </header>
+    </header>
+    <main class="p-4">
+      <RouterView/>
+    </main>
 
-  <RouterView />
+    <footer class="text-center text-gray-500 text-sm p-4 border-t border-gray-700">
+      © 2025 MovieApp.
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MovieList from "@/components/MovieList.vue";
+import MovieDetail from "@/components/MovieDetail.vue";
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import("../components/MovieList.vue"),
+  },
+  {
+    path: '/movie/:id',
+    name: 'MovieDetail',
+    component: () => import("../components/MovieDetail.vue"),
+    props: true,
+  },
+]
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about:id',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-
-  ],
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
 })
-
-export default router
